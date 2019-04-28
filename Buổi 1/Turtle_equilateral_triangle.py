5a8a3f5eebeabf5114045f4a6c37f20f44ecbf7d
mrd198 <50080807+mrd198@users.noreply.github.com>
2019-04-29 01:04:05 +0700

    Create Turtle_equilateral_triangle.py

---
+++ Buổi 1/Turtle_equilateral_triangle.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+shape('turtle')
+speed(-1)
+color("green")
+fillcolor("yellow")
+begin_fill()
+
+for v in range(3):
+    forward(200)
+    left(120)
+
+end_fill()
+
+mainloop()
