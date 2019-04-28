807f7364209c28677811e41a45f4dbb652866f60
mrd198 <50080807+mrd198@users.noreply.github.com>
2019-04-29 00:57:53 +0700

    Add files via upload

---
+++ Buổi 1/Turtle_Squa.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+shape('turtle')
+speed(-1)
+color("green")
+fillcolor("yellow")
+begin_fill()
+for v in range(2):
+    forward(200)
+    left(90)
+    forward(200)
+    left(90)
+end_fill()
+   
+
+mainloop()
