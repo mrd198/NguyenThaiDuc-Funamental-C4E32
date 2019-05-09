68becc4a0c03f594597e98fac2a7059090f42bf2
mrd198 <50080807+mrd198@users.noreply.github.com>
2019-05-09 23:44:10 +0700

    Create Turlte2.py

---
+++ Buổi 2/Turlte2.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+speed(-1)
+color('blue')
+
+for i in range(3):
+    forward(100)
+    left(120)
+
+for i in range(5):
+    forward(100)
+    left(72)
+
+color('red')
+for i in range(4):
+    forward(100)
+    left(90)
+
+for i in range(6):
+    forward(100)
+    left(60)
+    
+mainloop()    
