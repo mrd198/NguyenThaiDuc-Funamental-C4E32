a14fd50f359666fadc35c2287ddea3166399707a
mrd198 <50080807+mrd198@users.noreply.github.com>
2019-05-28 22:21:09 +0700

    Update 1.py

--- Buổi 7 - Tạo web/Bài 1/1.py
+++ Buổi 7 - Tạo web/Bài 1/1.py
@@ -2,37 +2,37 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/BMI/<w>/<h>')
+def tinh_BMI(w,h):
+    he = float(h) * 0.01
+    BMI = int(w) / (float(he) * float(he))
+    if BMI < 16:
+        return "BMI:"+str(BMI)+"<br> Severely underweight"
+    elif 16 <= BMI <= 18.5:
+        return "BMI:"+str(BMI)+"<br> Underweight"
+    elif 18.5 <= BMI <= 25:
+        return "BMI:"+str(BMI)+"<br> Normal"
+    elif 25 <= BMI <=30:
+        return "BMI:"+str(BMI)+"<br> Overweight"
+    else:
+        return "BMI:"+str(BMI)+"<br> Obese"
+
+
 # @app.route('/BMI/<int:w>/<int:h>')
 # def tinh_BMI(w,h):
 #     he = (h) * 0.01
 #     BMI = (w) / ((he) * (he))
 #     if BMI < 16:
-#         return "BMI:"+str(BMI)+"<br> Severely underweight"
+#         kq = "Severely underweight"
 #     elif 16 <= BMI <= 18.5:
-#         return "BMI:"+str(BMI)+"<br> Underweight"
+#         kq = "Underweight"
 #     elif 18.5 <= BMI <= 25:
-#         return "BMI:"+str(BMI)+"<br> Normal"
+#         kq = "Normal"
 #     elif 25 <= BMI <=30:
-#         return "BMI:"+str(BMI)+"<br> Overweight"
+#         kq = " Overweight"
 #     else:
-#         return "BMI:"+str(BMI)+"<br> Obese"
-
-
-@app.route('/BMI/<int:w>/<int:h>')
-def tinh_BMI(w,h):
-    he = (h) * 0.01
-    BMI = (w) / ((he) * (he))
-    if BMI < 16:
-        kq = "Severely underweight"
-    elif 16 <= BMI <= 18.5:
-        kq = "Underweight"
-    elif 18.5 <= BMI <= 25:
-        kq = "Normal"
-    elif 25 <= BMI <=30:
-        kq = " Overweight"
-    else:
-        kq = "Obese"
-    return render_template('bai1.html',data1 = BMI,data2 = kq)
+#         kq = "Obese"
+#     return render_template('bai1.html',data1 = BMI,data2 = kq)
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
